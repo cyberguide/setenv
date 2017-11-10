bacd42d1cae0136fda351b99e5992e0ac9fa1b7f
cyberguide <michael@ibbeken.com>
2017-11-10 09:09:24 +0100

    Update setenv.bat
    local location defined as .setenv

--- setenv.bat
+++ setenv.bat
@@ -2,7 +2,7 @@
 echo [%~n0] setenv
 
 rem local setenv location
-set local_setenv_dir=%~dp0.env
+set local_setenv_dir=%~dp0.setenv
 echo [%~n0] local_setenv_dir=%local_setenv_dir
 
 
@@ -17,6 +17,13 @@ rem determine local setenv folder
 echo [%~n0] test %local_setenv_dir%\setenv-%setenv_version%\setenv.bat
 if not exist %local_setenv_dir%\setenv-%setenv_version%\setenv.bat (
   echo [%~n0] copy setenv from remote repository
+  echo [%~n0] unpack setenv distribution into local setenv location  
 )
+
+
+rem delegate to pre-setenv script if available
+rem delegate to setenv distribution
+rem delegate to post-setenv script if available
+
 
 
